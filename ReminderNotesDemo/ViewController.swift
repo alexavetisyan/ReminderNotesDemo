24a84a2dc10a7839e6a790ed7a50ac2ba175c657
alexavetisyan <avetisyzn@gmail.com>
2023-07-22 00:20:14 +0400

    add ViewController and tableViewController add password on Folder

--- ReminderNotesDemo/ViewController.swift
+++ ReminderNotesDemo/ViewController.swift
@@ -8,11 +8,106 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    var timer = Timer()
+    var time = 4
+    
+    func xxx() {
         
-        print("hello")
+        time -= 1
+        if newFolder3.isHidden == false {
+            WarningLabel.isHidden = false
+        }
+        
+        timer = Timer.scheduledTimer(timeInterval: 1,
+                                     target: self,
+                                     selector: #selector(timefunc),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    @IBOutlet var collectionsOutlets: [UIView]!
+    @IBOutlet weak var WarningLabel: UILabel!
+    @IBOutlet weak var searchOutlet: UITextField!
+    @IBOutlet weak var TodayTask: UIView!
+    @IBOutlet weak var ScheduledOutlet: UIView!
+    @IBOutlet weak var AllOutlet: UIView!
+    @IBOutlet weak var CompletedOutlet: UIView!
+    @IBOutlet weak var PrivateOutlet: UIView!
+    @IBOutlet weak var addOutlet: UIButton!
+    @IBOutlet weak var newFolder1: UIView!
+    @IBOutlet weak var newFolder2: UIView!
+    @IBOutlet weak var newFolder3: UIView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    
+        TodayTask.layer.cornerRadius = 15
+        ScheduledOutlet.layer.cornerRadius = 15
+        AllOutlet.layer.cornerRadius = 15
+        CompletedOutlet.layer.cornerRadius = 15
+        PrivateOutlet.layer.cornerRadius = 15
+        newFolder1.layer.cornerRadius = 15
+        newFolder2.layer.cornerRadius = 15
+        newFolder3.layer.cornerRadius = 15
+        newFolder1.isHidden = true
+        newFolder2.isHidden = true
+        newFolder3.isHidden = true
+        WarningLabel.isHidden = true
+
+    }
+    @IBAction func todayAction(_ sender: Any) {
+    }
+    @IBAction func scheduledAction(_ sender: Any) {
+    }
+    @IBAction func allAction(_ sender: Any) {
+    }
+    @IBAction func completedAction(_ sender: Any) {
+    }
+    @IBAction func privateAction(_ sender: Any) {
+        
+    }
+    @IBAction func addAction(_ sender: Any) {
+        if newFolder1.isHidden == true {
+            newFolder1.isHidden = false
+        } else if newFolder2.isHidden == true {
+            newFolder2.isHidden = false
+        } else if newFolder3.isHidden == true {
+            newFolder3.isHidden = false
+        }
+        if collectionsOutlets.last?.isHidden == false {
+            WarningLabel.isHidden = false
+        }
+        
+        xxx()
+      
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+       
+        if WarningLabel.isHidden == false {
+            timefunc()
+        }
+    }
+    
+    @objc func timefunc() {
+        time -= 1
+        if time == 0 {
+            timer.invalidate()
+        }
+        
+        if time == 0 {
+            WarningLabel.isHidden = false
+        }
+        if WarningLabel.isHidden == false {
+            WarningLabel.isHidden = true
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        xxx()
+        
     }
 }
 
---
+++ TableViewController1.swift
@@ -0,0 +1,78 @@
+//
+//  TableViewController1.swift
+//  ReminderNotesDemo
+//
+//  Created by 1 on 21.07.23.
+//
+
+import UIKit
+
+class TableViewController1: UITableViewController {
+    var names = ""
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.tintColor = .darkGray
+        title = "My Private Folder"
+        tableView.backgroundView?.backgroundColor = .darkGray
+    }
+
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+       
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        return 0
+    }
+
+    
+    }
+   
+
+    
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+        
+    }
+}
+
