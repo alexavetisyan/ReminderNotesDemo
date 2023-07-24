ad3f0b2ab9aae8da22fb5dee438c91b0cef1a00d
alexavetisyan <avetisyzn@gmail.com>
2023-07-24 19:47:45 +0400

    aa

--- TableViewController1.swift
+++ TableViewController1.swift
@@ -55,12 +55,12 @@ class TableViewController1: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
        
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
        
-        return 0
+        return 3
     }
 
     
